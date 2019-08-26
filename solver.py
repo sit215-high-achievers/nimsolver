88833c45bcefd46ffd2c2c550f741c424ef94afa
Riley Baird <riley@mailo.com>
2019-08-26 10:24:25 +1000

    Update

--- solver.py
+++ solver.py
@@ -8,7 +8,36 @@ def check_empty(list):
 			return False
 	return True
 
+def check_win(list, misere, computer):
+	empty = check_empty(list)
+	if (empty and misere and not computer) or (empty and not misere and computer):
+		print("The computer won!")
+		exit()
+	elif (empty and not misere and not computer) or (empty and misere and computer):
+		print("You won!")
+		exit()
+
 def get_computer_move(list):
+	print_board(list)
+	print(group_nimsum(list))
+	if group_nimsum(list) == 0:
+		# Computer will lose unless user makes mistake
+		count = 0
+		while count < len(list):
+			if list[count] > 0:
+				list[count] = 0
+				break
+			count += 1
+	else:
+		individual_nimsums = [list[i] ^ group_nimsum(list) for i in range(len(list))]
+		count = 0
+		while count < len(list):
+			print(str(count) + ": " + str(individual_nimsums[count]) + " " + str(list[count]))
+			if individual_nimsums[count] < list[count]:
+				list[count] = individual_nimsums[count]
+				break
+			count += 1
+	return list
 
 def get_user_move(list):
 	# Print the current board
@@ -22,20 +51,23 @@ def get_user_move(list):
 		for x in range(len(list)):
 			if str(x) == pile:
 				pile = x
-				break
-		print("Invalid selection!")
-	while True:
-		numdiscs = input("How many discs do you want to take? ")
-		if numdiscs > 0 and numdiscs <= list[pile]:
+		if type(pile) == int and list[pile] > 0:
 			break
 		print("Invalid selection!")
+	while True:
+		try:
+			numdiscs = int(input("How many discs do you want to take? "))
+			if numdiscs > 0 and numdiscs <= list[pile]:
+				break
+		except:
+			print("Invalid selection!")
 
 	# Take the discs off the pile
 	list[pile] -= numdiscs
 	return list
 
 # The nimsum is the xor of the number of discs in each pile
-def nimsum(list):
+def group_nimsum(list):
 	sum = 0
 	for item in list:
 		sum ^= item
@@ -77,9 +109,6 @@ while count < numpiles:
 
 while True:
 	discs = get_user_move(discs)
-	empty = check_empty(discs)
-	if empty and misere:
-		print("The computer won!")
-	elif empty and not misere:
-		print("You won!")
+	check_win(discs, misere, False)
 	discs = get_computer_move(discs)
+	check_win(discs, misere, True)
